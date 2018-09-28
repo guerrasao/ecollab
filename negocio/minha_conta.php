2c2e3d29fa343638fb9f045ce5809d3a70ecf882
eliveltoncapra <43654597+eliveltoncapra@users.noreply.github.com>
2018-09-28 20:59:53 -0300

    minha_conta

--- negocio/minha_conta.php
+++ negocio/minha_conta.php
@@ -1,8 +1,9 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+session_start();
+$usuario=$_SESSION["usuario"];
+$dados=buscaCadastro($usuario);
+if($dados!=NULL){
+    //Tem informações
+}else{
+    //Erro
+}
