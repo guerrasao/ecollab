9fef75d2205748b5a5a3b656814fc6075ec01581
guerrasao <patryckguerra@yahoo.com.br>
2018-09-28 23:03:36 -0300

    verifica login

--- negocio/valida_login.php
+++ negocio/valida_login.php
@@ -9,6 +9,8 @@ if($teste==0){
     echo "Login feito com sucesso!";
     session_start();
     $_SESSION["usuario"]=$login;
+    $redirect = "inicio.php";
+    header("location:$redirect");
 }else if($teste==1){
     echo "Senha errada";
 }else{
