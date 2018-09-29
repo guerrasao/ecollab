2fdb1166426d2778752439bd8b78575f941d8917
guerrasao <patryckguerra@yahoo.com.br>
2018-09-29 05:19:34 -0300

    cadastra aprender area

--- cadastra_aprender_area.php
+++ cadastra_aprender_area.php
@@ -8,61 +8,38 @@
 
 <form class="container col-md-9" method="post" action="negocio/cadastro_aprender_area.php">
     <h1 class="p-3 mb-4">Quais áreas você tem interesse em aprender?</h1>
-    <div class="display-flex m-1">
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
+<?php
+include("banco/usuario.php");
+$matrizAreas= buscaCategorias();
+$i=0;
+while($dados= mysqli_fetch_array($matrizAreas)){
+    $id=$dados["id"];
+    $nome=$dados["nome"];
+    $foto=$dados["foto"];
+   
+    if($i == 0){
+        echo  '<div class="display-flex m-1">';
+    }
+        echo '
+            <div class="card box">
+                <div class="card-body d-flex flex-column">
+                    <input type="checkbox" class="m-1" name="area[]" value="'.$id.'">
+                    <img class="img-thumbnail m-1" src="img/'.$foto.'" alt="Card image cap">
+                    <h5 class="card-title m-1">'.$nome.'</h5>
+                </div>
             </div>
-        </div>
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
-            </div>
-        </div>
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
-            </div>
-        </div>
-    </div>
-    <div class="display-flex m-1">
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
-            </div>
-        </div>
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
-            </div>
-        </div>
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
-            </div>
-        </div>
-    </div>
-    <div class="display-flex m-1">
-        <div class="card box">
-            <div class="card-body d-flex flex-column">
-                <input type="checkbox" class="m-1" name="area[]" value="1">
-                <img class="img-thumbnail m-1" src="img/logotransparente.png" alt="Card image cap">
-                <h5 class="card-title m-1">Função de 1º grau</h5>
-            </div>
-        </div>
-    </div>
+        ';
+        ++$i;
+    if($i % 3 == 0 and $i>0){
+        echo '</div>';
+        echo  '<div class="display-flex m-1">';
+    }
+    //COLOCAR NA PARTE GRÁFICA 
+    
+}
+echo '</div>';
+?>
+       
     <div class="d-flex justify-content-end mt-4 mb-4">
         <button type="submit" name="submit" class="btn btn-primary pr-4 pl-4">Prosseguir</button>
     </div>
