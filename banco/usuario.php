7e808e2d638831e4216796220edbe7bf626d43ad
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2018-09-29 04:46:54 -0300

    idaprende

--- banco/usuario.php
+++ banco/usuario.php
@@ -150,9 +150,9 @@
     
     function cadastraNotificacao($usuarioAprende, $idEnsina){
         $con=abrirConexao();
-        $result= mysqli_query($con, "SELECT id FROM usuario WHERE usuario='$usuario'");
+        $result= mysqli_query($con, "SELECT id FROM usuario WHERE usuario='$usuarioAprende'");
         $dados= mysqli_fetch_array($result);
-        $idEnsina=$dados["id"];
+        $idAprende=$dados["id"];
         $result=mysqli_query($con,"INSERT INTO notificacao_ensino(usuario_id_ensina, usuario_id_aprende) VALUES ($idEnsina, $idAprende)");
     }
     
