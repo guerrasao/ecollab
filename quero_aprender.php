cc9f9cb08bfcbe6accdc1eaf8dcfb73608e133c6
guerrasao <patryckguerra@yahoo.com.br>
2018-09-29 03:57:07 -0300

    quero aprender

--- quero_aprender.php
+++ quero_aprender.php
@@ -5,39 +5,67 @@
     $file = $path . 'cabecalho_usuario_logado.php';
     include_once($file);
 ?>
-
 <section class="container p-3">
     <div class="form-group">
-        <a class="btn btn-primary col-md-12 p-3 ml-1 mt-4 mb-4" href="cadastro_aprender_are.php">Cadastrar Interesse</a>
+        <a class="btn btn-primary col-md-12 p-3 ml-1 mt-4 mb-4" href="cadastro_aprender_area.php">Cadastrar Interesse</a>
     </div>
     <form class="form-row pb-4 align-content-md-between ml-1" action="" method="post">
         <input type="text" class="form-control col-md-9" id="pesquisa" name="pesquisa" placeholder="Interesse">
         <button type="submit" name="submit" class="btn btn-primary  mr-4 ml-4 pr-4 pl-4  col-md-2">Pesquisar</button>
     </form>
+
+<?php
+    include("../banco/usuario.php");
+    session_start();
+    $usuario=$_SESSION["usuario"];
+    $ids=buscaPorInteresseAprender($usuario);
+
+    if($ids!=NULL){
+        for($i=0;$i<count($ids);$i++){
+            $dados= buscaInfoUsuario($ids[$i]);
+            $ensinar= buscaQuerEnsinar($dados["id"]);
+            $aprender= buscaQuerAprender($dados["id"]);
+?>
     <div class="container-fluid row jumbotron m-1 mt-3 align-items-center pb-2 pt-4 pr-4">
         <div class="col-md-1">
             <img src="..." class="img-fluid" alt="Foto do usuário">
         </div>
         <div class="col-md-2">
-            <p>Nome da pessoa</p>
-        </div>
+            <p><?php echo $dados["nome"]; ?></p>
+        </div>    
         <div class="col-md-6">
             <table class="table bg-white">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="col">Pode Ensinar</th>
-                  <th scope="col">Pode Aprender</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Matemática</td>
-                  <td>Matemática</td>
-                </tr>
-                <tr>
-                  <td>Matemática</td>
-                  <td>Matemática</td>
-                </tr>
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Pode Ensinar</th>
+                        <th scope="col">Pode Aprender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+<?php
+            //html - nome;
+            if($ensinar!=NULL){
+                foreach ($ensinar as $ensinar['nome'] => $value) {
+                    echo "<td>".$value."</td>";
+                }
+            }else{
+                echo "<td>Não quer ensinar nada.</td>";
+            }
+?>
+                    </tr>
+                    <tr>
+<?php
+            if($aprender!=NULL){
+                foreach($aprender as $aprender['nome'] => $value) {
+                    echo "<td>".$value."</td>";
+                    //html - conteudo que quer aprender;
+                }
+            }else{
+                echo "<td>Não quer aprender nada.</td>";
+            }
+?>
+                    </tr>
               </tbody>
             </table>
         </div>
@@ -45,37 +73,16 @@
             <a class="btn btn-primary col-md-11 " href="demonstrar_interesse.php">Demonstrar Interesse</a>
         </div>
     </div>
-    <div class="container-fluid row jumbotron m-1 mt-3 align-items-center pb-2 pt-4 pr-4">
-        <div class="col-md-1">
-            <img src="..." class="img-fluid" alt="Foto do usuário">
-        </div>
-        <div class="col-md-2">
-            <p>Nome da pessoa</p>
-        </div>
-        <div class="col-md-6">
-            <table class="table bg-white">
-              <thead class="thead-dark">
-                <tr>
-                  <th scope="col">Pode Ensinar</th>
-                  <th scope="col">Pode Aprender</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Matemática</td>
-                  <td>Matemática</td>
-                </tr>
-                <tr>
-                  <td>Matemática</td>
-                  <td>Matemática</td>
-                </tr>
-              </tbody>
-            </table>
-        </div>
-        <div class="col-md-3">
-            <a class="btn btn-primary col-md-11 " href="demonstrar_interesse.php">Demonstrar Interesse</a>
-        </div>
-    </div>
+<?php
+                        
+        }
+    }else{
+        echo '<div class="alert alert-danger" role="alert">
+                Nenhum elemento similar!
+              </div>';
+    }
+?>
+
 </section>
 
 <?php
