10e707d52db2385460d84a1fc6ab5cca4e858f82
guerrasao <patryckguerra@yahoo.com.br>
2018-09-28 23:52:31 -0300

    teste 5

--- minha_conta.php
+++ minha_conta.php
@@ -11,7 +11,7 @@ include("banco/usuario.php");
 session_start();
 $usuario=$_SESSION["usuario"];
 $dados=buscaCadastro($usuario);
-echo "aqui ".$dados["nome"];
+echo "aqui usuario=".$usuario.", dados".$dados["nome"];
 if($dados!=NULL){
     
 ?>
