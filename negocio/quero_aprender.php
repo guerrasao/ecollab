b4e2a0a2d2d11e2040907aff56c4518229f98880
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2018-09-28 22:55:09 -0300

    envio

--- negocio/quero_aprender.php
+++ negocio/quero_aprender.php
@@ -1,12 +1,13 @@
 <?php
-include("banco/usuario.php");
+include("../banco/usuario.php");
 session_start();
 $usuario=$_SESSION["usuario"];
 $ids=buscaPorInteresseAprender($usuario);
 if($ids!=NULL){
     for($i=0;$i<count($tam);$i++){
-        $dados=buscaInfoInteresseAprender($id[$i]);  
+        $dados=buscaInfoInteresseAprender($ids[$i]);  
     }
 }else{
+    echo "nenhum";
     //Nenhum elemento similar;
 }
