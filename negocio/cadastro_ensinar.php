bb35341649d25d2aa8252b3e46e3d46d4f53cc9f
eliveltoncapra <43654597+eliveltoncapra@users.noreply.github.com>
2018-09-28 23:18:27 -0300

    cadastro_ensinar/cadastro_interesse

--- negocio/cadastro_ensinar.php
+++ negocio/cadastro_ensinar.php
@@ -3,9 +3,9 @@
 include("../banco/usuario.php");
 $matrizAreas=SolicitAreas();
 
-for($i=0;$i<count($matrizAreas);$i++){
+for($i=0;$i<count($matrizAreas); $i++){
     $id=$matrizAreas[$i]["id"];
     $nome=$matrizAreas[$i]["nome"];
     $foto=$matrizAreas[$i]["foto"];
-    //COLOCAR NA PARTE GRÁFICA 
+    //
 }
