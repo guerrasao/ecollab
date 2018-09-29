781590f7785036c67b3a10739376d1561c7811a4
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2018-09-29 08:29:55 -0300

    notificacoes

--- negocio/exibe_notificacoes.php
+++ negocio/exibe_notificacoes.php
@@ -9,7 +9,29 @@
             $id; //id da notificacão guardar infomação
             $nome=$dados["nome"];
             $idContato=$dados["id"];
-            echo "ID NOTIFICAÇÃO: $id | NOME DO CONTATO: $nome | ID CONTATO: <br>";
+            echo "ID NOTIFICAÇÃO: $id | NOME DO CONTATO: $nome | ID CONTATO: $idContato <br><br>";
+            $dados= buscaInfoUsuario($idContato);
+            $ensinar= buscaQuerEnsinar($idContato);
+            $aprender= buscaQuerAprender($idContato);
+            //html - nome;
+            if($ensinar!=NULL){
+                foreach ($ensinar as $ensinar['nome'] => $value) {
+                    echo "$value <br>";
+                    //html - conteudo que quer ensinar;
+                }
+            }else{
+                "Não quer ensinar nada.";
+            }
+
+            if($aprender!=NULL){
+                foreach($aprender as $aprender['nome'] => $value) {
+                    echo "$value <br>";
+                    //html - conteudo que quer aprender;
+                }
+            }else{
+                echo "não quer aprender nada.";
+            }
+            
         }
     }else{
         echo "Você não possui notificações!";
