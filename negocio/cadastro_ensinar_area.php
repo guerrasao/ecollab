12a2208f157fa4ebb31976095beef7c17a82a384
DanielSeitenfus <daniel.seitenfus@hotmail.com>
2018-09-28 23:40:18 -0300

    mudança buscaCategorias

--- negocio/cadastro_ensinar_area.php
+++ negocio/cadastro_ensinar_area.php
@@ -1,9 +1,8 @@
 <?php
 
 include("../banco/usuario.php");
-$matrizAreas=SolicitAreas();
-
-for($i=0;$i<count($matrizAreas); $i++){
+$categorias=buscaCategorias();
+for($i=0;$i<count($categorias); $i++){
     $id=$matrizAreas[$i]["id"];
     $nome=$matrizAreas[$i]["nome"];
     $foto=$matrizAreas[$i]["foto"];
